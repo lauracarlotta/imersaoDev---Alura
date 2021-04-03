3d8d7edd5deb8ab82362041494af6aa90af77cc9
lauracarlotta <carlotta.custodio@gmail.com>
2021-04-03 04:29:02 -0300

    add classList by JS, it refactored my code, validated errors

--- projects/day3/challenge/the_mentalist_with_challenges/index.js
+++ projects/day3/challenge/the_mentalist_with_challenges/index.js
@@ -1,42 +1,77 @@
-let answerNumber = document.querySelector(".answer");
+alert("Você terá 3 chances valendo 5 pontos cada uma!");
+
+let answerNumber = document.querySelector(".answer-none");
 let untilWhatNumber = Number(prompt("Você quer que o sorteio seja de 1 a que número? "));
-let numeroSecreto = Math.round(Math.random() * untilWhatNumber);
+let secretNumber = Math.round(Math.random() * untilWhatNumber);
+let prizeDrawGuess = 1;
+let point = 15;
+let totalAttempts = 3;
+let guess = 0;
 
-let totalTentativas = 3;
-let chute = 0;
+while (secretNumber == 0) {
 
-if (numeroSecreto == 0) {
-
-    numeroSecreto = 1; // Para que o número sorteado não seja 0
+    secretNumber = Math.round(Math.random() * untilWhatNumber);
+    prizeDrawGuess++;
 }
 
-console.log(numeroSecreto);
+console.log(secretNumber);
 
-while (totalTentativas > 0 && numeroSecreto != 0) {
+while (totalAttempts > 0) {
 
-    chute = parseFloat(Number(prompt(`Chute um um número entre 1 e ${untilWhatNumber}!`)));
+    guess = parseFloat(Number(prompt(`Chute um um número entre 1 e ${untilWhatNumber}!`)));
 
-    if (numeroSecreto == chute) {
-        
-        answerNumber.innerHTML = `<strong>Você acertou!</strong>`;
-        totalTentativas = 0;
+    if (guess <= 0 || guess > untilWhatNumber) {
 
-    } else if (numeroSecreto > chute) {
+        alert(`Valor inválido!`);
+        alert(`Você não perdeu tentativas, mas precisa chutar um número entre 1 e ${untilWhatNumber}!`);
 
-        answerNumber.innerText = "O número secreto é maior!";
-        totalTentativas--;
+    } else {
 
-    } else if (numeroSecreto < chute) {
+        if (secretNumber == guess) {
+            
+            answerNumber.classList.remove("answer-none");
+            answerNumber.innerHTML = `<strong>Você acertou!</strong>`;
+            break;
 
-        answerNumber.innerText = "O número secreto é menor!";
-        totalTentativas--;
+        } else if (secretNumber > guess && guess != 0 && guess < untilWhatNumber) {
+
+            answerNumber.classList.remove("answer-none");
+            answerNumber.innerText = "O número secreto é maior!";
+            totalAttempts--;
+            point-=5;
+
+
+        } else if (secretNumber < guess && guess != 0 && guess < untilWhatNumber) {
+
+            answerNumber.classList.remove("answer-none");
+            answerNumber.innerText = "O número secreto é menor!";
+            totalAttempts--;
+            point-=5;
+        }
+
     }
+
 }
 
-if (chute != numeroSecreto) {
+if (totalAttempts == 3){
 
-    answerNumber.innerHTML = `<em> Suas tentativas acabaram! </em> <br> <strong> O número secreto era ${numeroSecreto}!</strong>`;
+    answerNumber.classList.remove("answer-none");
+    answerNumber.innerHTML += `<br>Caraca, você é bom! <br> Por ser de primeira, <br> <em> <strong> <big> ganhou ${point} pontos! </em> </strong> </big>`;
+    
+} else if (totalAttempts == 2){
+
+    answerNumber.classList.remove("answer-none");
+    answerNumber.innerHTML += `<br> Acertando de segunda, <br> <em> <strong> <big> ganha ${point} pontos! </em> </strong> </big>`;
+
+} else if (totalAttempts == 1) {
+
+    answerNumber.classList.remove("answer-none");
+    answerNumber.innerHTML += `<br> Ufffffa... Foi quase!!! <em> <strong> <big> Ganhou ${point} pontos! </em> </strong> </big>`;
+
 }
 
+if (guess != secretNumber) {
 
-
+    answerNumber.classList.remove("answer-none");
+    answerNumber.innerHTML = `<em> Suas tentativas acabaram! </em> <br> <strong> Você não ganhou nenhum ponto... <br> O número secreto era ${secretNumber}!</strong>`;
+}
