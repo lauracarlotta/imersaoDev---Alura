327468188a6109364cd7b2571cdec44fc1355966
lauracarlotta <carlotta.custodio@gmail.com>
2021-04-06 20:54:36 -0300

    day 4 - undertanding arrays

--- explanations/day4.js
+++ explanations/day4.js
@@ -1,3 +1,27 @@
 // let = única variável;
 // array = lista de variaveis (pode receber uma lista);
-// .push (empurra) para dentro da nossa lista
+// .push() (empurra) para dentro da nossa lista;
+// .length (tamanho do array);
+// os índices nos array sempre começam de 0, ou seja, se o nosso array tiver 4 elementos, seu índice será de 0 a 3, ou seja, 0, 1, 2 e 3 = 4 elementos;
+
+let movies = ["Batman e o Ca valeiro das Trevas", "Matrix", "Interstellar"];
+//                  0           1           2
+movies.push("Forest Gump");
+//              3
+console.log(movies);
+console.log(movies.length);
+console.log(movies[4]);
+
+/* 
+let indice = 0
+
+ while (indice < movies.length) {
+    
+    console.log(movies[indice]);
+    indice++
+} 
+*/
+
+for(let indice = 0; indice < movies.length; indice++) {
+    console.log(movies[indice]);
+}
--- projects/day4/alura_flix/index.js
+++ projects/day4/alura_flix/index.js
@@ -1,3 +1,30 @@
-let movies = ["Star Wars", "Matrix", "Interstellar"];
+let moviesListImage = [];
+let moviesListName = [];
+let movieName = document.write();
 
-console.log(movies);
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg");
+moviesListName.push("Batman Dark Knight");
+ 
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg");
+moviesListName.push("Matrix");
+
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg");
+moviesListName.push("Interstellar");
+ 
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BNWIwODRlZTUtY2U3ZS00Yzg1LWJhNzYtMmZiYmEyNmU1NjMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR1,0,182,268_AL_.jpg");
+moviesListName.push("Forest Gump");
+
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BNDE4OTMxMTctNmRhYy00NWE2LTg3YzItYTk3M2UwOTU5Njg4XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg");
+moviesListName.push("Schindler's List");
+
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BOTdiNzJlOWUtNWMwNS00NmFlLWI0YTEtZmI3YjIzZWUyY2Y3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg");
+moviesListName.push("Requiem for a Dream");
+
+moviesListImage.push("https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX182_CR0,0,182,268_AL_.jpg");
+moviesListName.push("Inception");
+
+for (let i = 0; i < moviesListImage.length; i++) {
+    document.write(`<img src = "${moviesListImage[i]}" >`);
+    
+    document.write(`${moviesListName[i]}`);
+}
